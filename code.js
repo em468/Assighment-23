d3ae42065e06c8a047265c77ccb3dd2e9f6c680e
em468 <116311548+em468@users.noreply.github.com>
2022-12-13 15:09:41 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,13 @@
+function hello(name) {
+    alert ("hello"+ name);
+}
+
+hello (" Can you find the secert message");
+
+
+
+function secertMessage(message) {
+    console.log("The secret message is " + message);
+
+}
+hello (" i love my cats <3"); 
